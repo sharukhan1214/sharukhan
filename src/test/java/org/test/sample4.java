4017b89a8d6d4dff2c3646f09939cac888d77ffe
sharukhan1214 <amjathsharu64@gmail.com>
2022-05-26 23:12:42 +0530

    message

---
+++ src/test/java/org/test/sample4.java
@@ -0,0 +1,16 @@
+package org.test;
+
+public class sample4 {
+	
+		private void method1() {
+			System.out.println("1");
+
+		}
+		private void method2() {
+			System.out.println("2");
+		}
+		private void method3() {
+			System.out.println("3");
+		}
+	}
+
--- src/test/java/sampleClass.java
+++ src/test/java/sampleClass.java
@@ -1,4 +1,15 @@
 
 public class sampleClass {
+private void method1() {
+	System.out.println("1");
+
+}
+private void method2() {
+	System.out.println("2");
+}
+private void method3() {
+	System.out.println("3");
+
+}
 
 }
