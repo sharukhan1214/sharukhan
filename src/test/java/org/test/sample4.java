041990f71716a814e00c2cac6598aba74aff6b7c
Ganesh2803 <ganeshzeal123@gmail.com>
2022-05-27 14:53:39 +0530

    Ganesh added methods

--- src/test/java/org/test/sample4.java
+++ src/test/java/org/test/sample4.java
@@ -12,5 +12,21 @@ public class sample4 {
 		private void method3() {
 			System.out.println("3");
 		}
+		
+		
+		private void dhoni() {
+			System.out.println("1");
+
+		}
+		
+		private void virat() {
+			System.out.println("2");
+
+		}
+		
+		private void rohit() {
+		System.out.println("3");
+
+		}
 	}
 
