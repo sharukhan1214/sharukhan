1cee11d63e342fd3e193f2bf001092f3d44f981d
sharukhan1214 <amjathsharu64@gmail.com>
2022-05-27 15:15:22 +0530

    sharu methods1

--- src/test/java/org/test/sample4.java
+++ src/test/java/org/test/sample4.java
@@ -12,5 +12,17 @@ public class sample4 {
 		private void method3() {
 			System.out.println("3");
 		}
+		private void bilal() {
+			System.out.println("bilal");
+
+		}
+		private void sharuk() {
+			System.out.println("sharuk");
+
+		}
+		private void ganesh() {
+			System.out.println("ganesh");
+
+		}
 	}
 
